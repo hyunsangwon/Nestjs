73e2f90bb9fb784250c2d7c3bea8d638f889ce12
HyunSangWon <hyunsangwon93@gmail.com>
2021-06-01 11:42:10 +0900

    fix(app) : app servcie 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { config, AWSConfig } from '../config';
 import { UserModule } from './user/user.module';
 import { DeviceModule } from './device/device.module';
@@ -10,6 +9,6 @@ import { AwsSdkModule } from 'nest-aws-sdk';
 @Module({
     imports: [TypeOrmModule.forRoot(config), AwsSdkModule.forRoot(AWSConfig), UserModule, DeviceModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [],
 })
 export class AppModule {}
